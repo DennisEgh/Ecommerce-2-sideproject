83d32b85d0bb202103bce6d5c9504e15a7084830
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-08 17:08:32 +0100

    added some general css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-    <h1>hello</h1>
+    <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <h1>hellothere</h1>
+    );
+}
+
+export default Home;
