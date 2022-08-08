2922ccbdabd4f5bbbe4a8a9f2c11be51ae965d55
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-09 00:45:11 +0100

    jkbkhi i

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Nav />
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import video from "../assets/landingpagevid.mp4"
+
 
 const Landing = () => {
   return (
     <section id="landing">
+   
+    <video autoPlay muted loop id="landingvideo">
+      <source src={video} type="video/mp4"></source>
+      Your browser does not support the video tag.
+    </video>
       <header>
       
       </header>
