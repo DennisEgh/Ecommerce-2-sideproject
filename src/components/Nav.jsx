075bb67ac23525e803443d9aca17cf0d077dde36
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-09 23:45:32 +0100

    sdamkdads

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,15 +10,16 @@ const Nav = () => {
         <Link to="/">
           <img className="logo" src={logo} alt="" />
         </Link>
-        <ul className="nav__links">
-            <li className="nav__list">
-                <Link className="nav__link" to="/">WOMENS</Link>
+        <ul className="nav__links ">
+            <li className="nav__list nav__list--women">
+              <div className="nav__list--menu"></div>
+                <Link className="nav__link nav__link--middle" to="/">WOMENS</Link>
             </li>
             <li className="nav__list">
-                <Link className="nav__link" to="/">SHOES</Link>
+                <Link className="nav__link nav__link--middle" to="/">SHOES</Link>
             </li>
             <li className="nav__list">
-                <Link className="nav__link" to="/">ACCESSORIES</Link>
+                <Link className="nav__link nav__link--middle" to="/">ACCESSORIES</Link>
             </li>
         </ul>
         <ul className="nav__links">
