e0827bfcc9c8850afc5f7c6e8c207e57e19f2f38
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-10 00:30:13 +0100

    save

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-
+import cart from "../assets/cart.webp";
 
 const Nav = () => {
   return (
@@ -11,33 +11,46 @@ const Nav = () => {
           <img className="logo" src={logo} alt="" />
         </Link>
         <ul className="nav__links ">
-            <li className="nav__list nav__list--women">
-              <div className="nav__list--menu">
-                <h1>eeee</h1>
-              </div>
-                <Link className="nav__link nav__link--middle" to="/">WOMENS</Link>
-            </li>
-            <li className="nav__list nav__list--shoes">
+          <li className="nav__list nav__list--women">
+            <div className="nav__list--menu">
+              <h1>eeee</h1>
+            </div>
+            <Link className="nav__link nav__link--middle" to="/">
+              WOMENS
+            </Link>
+          </li>
+          <li className="nav__list nav__list--shoes">
             <div className="nav__list--menu">
               <h1>oooo</h1>
             </div>
-                <Link className="nav__link nav__link--middle" to="/">SHOES</Link>
-            </li>
-            <li className="nav__list nav__list--accessories">
+            <Link className="nav__link nav__link--middle" to="/">
+              SHOES
+            </Link>
+          </li>
+          <li className="nav__list nav__list--accessories">
             <div className="nav__list--menu "></div>
-                <Link className="nav__link nav__link--middle" to="/">ACCESSORIES</Link>
-            </li>
+            <Link className="nav__link nav__link--middle" to="/">
+              ACCESSORIES
+            </Link>
+          </li>
         </ul>
-        <ul className="nav__links">
-            <li className="nav__list">
-                <Link className="nav__link" to="/">CART</Link>
-            </li>
-            <li className="nav__list">
-                <Link className="nav__link" to="/">ACCOUNT</Link>
-            </li>
+        <ul className="nav__links nav__links--end">
+          <li className="nav__list">
+            <Link className="nav__link" to="/">
+              <img className="cart__logo" src={cart} alt="" />
+              <div className="cart__length">
+                <div className="cart__para">1</div>
+              </div>
+              
+            </Link>
+          </li>
+          <li className="nav__list">
+            <Link className="nav__link" to="/">
+              ACCOUNT
+            </Link>
+          </li>
         </ul>
       </div>
-      
     </nav>
   );
 };
