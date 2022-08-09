50ca28af08bf734211750e9fd60be27884ed29ea
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-10 00:02:23 +0100

    nice

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,13 +12,19 @@ const Nav = () => {
         </Link>
         <ul className="nav__links ">
             <li className="nav__list nav__list--women">
-              <div className="nav__list--menu"></div>
+              <div className="nav__list--menu">
+                <h1>eeee</h1>
+              </div>
                 <Link className="nav__link nav__link--middle" to="/">WOMENS</Link>
             </li>
-            <li className="nav__list">
+            <li className="nav__list nav__list--shoes">
+            <div className="nav__list--menu">
+              <h1>oooo</h1>
+            </div>
                 <Link className="nav__link nav__link--middle" to="/">SHOES</Link>
             </li>
-            <li className="nav__list">
+            <li className="nav__list nav__list--accessories">
+            <div className="nav__list--menu "></div>
                 <Link className="nav__link nav__link--middle" to="/">ACCESSORIES</Link>
             </li>
         </ul>
