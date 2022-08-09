b8fc705096c445d041f67b4025ad254e7df49251
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-09 20:09:32 +0100

    added more wstuff

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
-
-
-
 const items = [
-
-<div className="item pic--one" data-value="1">
+  <div className="item pic--one" data-value="1">
     <h1>hello</h1>
-</div>,
+  </div>,
 
-
-<div className="item pic--two" data-value="2">
-    
-</div>
-
+  <div className="item pic--two" data-value="2"></div>,
 ];
 
 const Gallery = () => {
-  return <AliceCarousel mouseTracking={false} items={items} />;
+  return (
+    <AliceCarousel
+      mouseTracking={false}
+      items={items}
+      autoPlay
+      autoPlayInterval={3000}
+      infinite
+      animationDuration={1500}
+    />
+  );
 };
 export default Gallery;
