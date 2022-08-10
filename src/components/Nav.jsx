12e87f3b63dac0c33a7c9290021af296dcb08a85
Dennis Eghtedari <year_2000_@hotmail.com>
2022-08-11 00:16:52 +0100

    save

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import cart from "../assets/cart.webp";
-import hamburger from "../assets/Hamburger_icon.svg"
+import hamburger from "../assets/Hamburger_icon.svg";
 const Nav = () => {
   return (
     <nav>
@@ -12,24 +12,20 @@ const Nav = () => {
         </Link>
         <ul className="nav__links ">
           <li className="nav__list nav__list--women">
-            <div className="nav__list--menu">
-              <h1>eeee</h1>
-            </div>
             <Link className="nav__link nav__link--middle" to="/shop">
               WOMENS
             </Link>
           </li>
           <li className="nav__list nav__list--shoes">
-            <div className="nav__list--menu">
-              <h1>oooo</h1>
-            </div>
             <Link className="nav__link nav__link--middle" to="/">
               FRAGRANCES
             </Link>
           </li>
           <li className="nav__list nav__list--accessories">
-            <div className="nav__list--menu "></div>
-            <Link className="nav__link nav__link--middle" to="/">
+            <Link
+              className="nav__link nav__link--middle"
+              to="/shop-accessories"
+            >
               ACCESSORIES
             </Link>
           </li>
@@ -48,10 +44,7 @@ const Nav = () => {
             </Link>
           </li>
           <li className="nav__list burger__menu">
-            <img
-              src={hamburger}
-              alt=""
-            />
+            <img src={hamburger} alt="" />
           </li>
         </ul>
       </div>
--- src/components/ui/Landingcard.jsx
+++ src/components/ui/Landingcard.jsx
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Landingcardprice from "./Landingcardprice";
 
 const Landingcard = ({ ads }) => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = ads.url;
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, []);
+    };
+  });
   return (
-    <div className="card">
-      <Link className="card__link" to={`/shop/${ads.id}`}>
-        <img className="card__img" src={ads.url} alt="" />
-        <div className="card__description">
-          {ads.new ? (
-            <div className="card__description--top">
-              <p className="description__para--top">NEW</p>
-            </div>
-          ) : (
-            <div className="card__description--top">
-              <p className="description__para--top inactive">NEW</p>
-            </div>
-          )}
-          <Landingcardprice
-            salePrice={ads.salePrice}
-            originalPrice={ads.originalPrice}
-          />
-          <p className="description__para--mid">{ads.maker}</p>
-          <p className="description__para--bot">{ads.title}</p>
+    <>
+      {img ? (
+        <>
+          <div className="card">
+            <Link className="card__link" to={`/shop/${ads.id}`}>
+              <img className="card__img" src={ads.url} alt="" />
+              <div className="card__description">
+                {ads.new ? (
+                  <div className="card__description--top">
+                    <p className="description__para--top">NEW</p>
+                  </div>
+                ) : (
+                  <div className="card__description--top">
+                    <p className="description__para--top inactive">NEW</p>
+                  </div>
+                )}
+                <Landingcardprice
+                  salePrice={ads.salePrice}
+                  originalPrice={ads.originalPrice}
+                />
+                <p className="description__para--mid">{ads.maker}</p>
+                <p className="description__para--bot">{ads.title}</p>
+              </div>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <div className="card">
+          <div className="skeleton">
+           
+          </div>
         </div>
-      </Link>
-    </div>
+      )}
+    </>
   );
 };
 
